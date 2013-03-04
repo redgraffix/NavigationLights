33b4fec1e37c57020eb4aaa5364c6cdbb3a499a3
Jason Rodgers <red@redgraffix.com>
2013-03-04 13:59:29 -0500

    Update navlights_js.js

--- js/navlights_js.js
+++ js/navlights_js.js
@@ -5,15 +5,19 @@
  *  Licensed under the MIT license
  *
  */
-function navlights(){
-  var DocScrollTop = $(document).scrollTop();
-	$(".section").each(function(i) {
-		var sectionOffsetTop = $(this).offset();
-		var navnum = $(this).index();	
-			if (sectionOffsetTop.top - ($(this).outerHeight(true)*.5) <= DocScrollTop 
-			&& sectionOffsetTop.top + ($(this).outerHeight(true)*.5) >= DocScrollTop)
-			{$("#navlight"+navnum+"").addClass("nav_active");}
-		else {$("#navlight"+navnum+"").removeClass("nav_active");};
-	});	
-	};
-$(document, window).on("ready, scroll", (function() {navlights();}));
+function NavLights() {
+	var sectId = 0;
+	$(".section").each(function() {
+		sectId = sectId+1;
+		$(this).data("sectId", sectId);
+		});
+	$(document).scroll(function() {
+		var DocScrollTop = $(document).scrollTop();
+			$(".section").each(function() {
+				var sectionOffsetTop = $(this).offset();		
+				if (sectionOffsetTop.top - ($(this).outerHeight(true)*.1) <= DocScrollTop && sectionOffsetTop.top + ($(this).outerHeight(true)*.9) >= DocScrollTop){$("#navlight"+$(this).data("sectId")+"").addClass("nav_active");}
+				else {$("#navlight"+$(this).data("sectId")+"").removeClass("nav_active");};	
+			});	
+		});
+};
+$(NavLights);	
